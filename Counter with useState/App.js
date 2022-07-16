839786651eff6a1117ea9da99a5c2d28dc6d4ad6
Ömer Yusuf SORHUN <55853041+omer3458@users.noreply.github.com>
2022-07-16 20:43:58 +0300

    Counter Example with useState

---
+++ Counter with useState/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState,useReducer } from 'react';
+import "./index.css";
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Counter from './components/Counter';
+
+
+
+function App() {
+ 
+  return (
+    <>
+    <Counter>
+
+    </Counter>
+    </>
+    
+    
+
+  );
+}
+
+export default App;
