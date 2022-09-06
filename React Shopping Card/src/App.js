9cc62c97d29c395e369cb5e35216a8a24740158e
Omer Yusuf <omersorgun34@hotmail.com>
2022-09-06 17:05:02 +0300

    Shopping Card with 128 Billion Dollars

--- React Shopping Card/src/App.js
+++ React Shopping Card/src/App.js
@@ -1,29 +1,41 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import Header from "./components/Header";
-import products from "./products.json";
-import "./index.css";
-import Product from "./components/Product";
+import './App.css';
+import {useState, useEffect} from 'react'
+import Header from './components/Header';
+import Product from './components/Product';
+import Basket from './components/Basket';
+import products from './products.json'
 
 function App() {
-  const [money, setMoney] = useState(100);
-  const [basket, setBasket] = useState([]);
 
-  useEffect(() => {
-    console.log(basket);
-  }, [basket]);
-  return (
-    <div className="App">
-      <Header money={money} />
-      {products.map((product) => (
-        <Product
-          key={product.id}
-          basket={basket}
-          setBasket={setBasket}
-          product={product}
-        />
-      ))}
-    </div>
-  );
+	const [money, setMoney] = useState(128000000000)
+	const [basket, setBasket] = useState([])
+	const [total, setTotal] = useState(0)
+
+	const resetBasket = () => {
+		setBasket([])
+	}
+
+	useEffect(() => {
+		setTotal(
+			basket.reduce((acc, item) => {
+				return acc + (item.amount * (products.find(product => product.id === item.id).price))
+			}, 0),
+		)
+	}, [basket])
+
+	return (
+		<>
+			<Header total={total} money={money}/>
+			<div className="container products">
+				{products.map(product => (
+					<Product key={product.id} total={total} money={money} basket={basket} setBasket={setBasket} product={product}/>
+				))}
+			</div>
+			{total > 0 && (
+				<Basket resetBasket={resetBasket} products={products} total={total} basket={basket} />
+			)}
+		</>
+	);
 }
+
 export default App;
